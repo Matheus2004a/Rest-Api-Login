3b325bf524d36fc04c34b755e42c8c142c4bdadf
Matheus Aurélio <matheusaurelio2004@gmail.com>
2023-01-23 23:58:45 -0300

    :sparkles: feat: Add page Not Found

---
+++ frontend/src/pages/Error/NotFound.jsx
@@ -0,0 +1,12 @@
+import "./style.scss"
+
+function NotFound() {
+    return (
+        <main>
+            <h1>Error 404</h1>
+            <p>Página inexistente</p>
+        </main>
+    ) 
+}
+
+export default NotFound
